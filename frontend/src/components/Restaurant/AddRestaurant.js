725780fdec6421cc01c826b83b96bc24471dfe97
vksoni10 <vanshksoni@gmail.com>
2024-07-06 19:57:43 +0530

    add restaurant

--- frontend/src/components/Restaurant/AddRestaurant.js
+++ frontend/src/components/Restaurant/AddRestaurant.js
@@ -1,5 +1,22 @@
 import React from "react";
+import "./AddRestaurant.css";
 
 export default function AddRestaurant() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <div>
+          <h2>Unlock a new revenue stream.</h2>
+          <h4>
+            Potato's global platform gives you the flexibility, visibility and
+            customer insights you need to connect with more customers. Partner
+            with us today
+          </h4>
+        </div>
+        <div>
+          <form></form>
+        </div>
+      </div>
+    </>
+  );
 }
--- frontend/src/components/Restaurant/ViewRestaurant.js
+++ frontend/src/components/Restaurant/ViewRestaurant.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ViewRestaurant.css";
 
 export default function ViewRestaurant() {
   return <></>;
