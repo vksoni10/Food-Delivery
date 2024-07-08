83e9644c9942980570d18359cf054150ccd2db7a
KD-53 <krishnadadhich30@gmail.com>
2024-07-08 20:51:28 +0530

    hello

--- backend/routes/restRoute.js
+++ backend/routes/restRoute.js
@@ -2,11 +2,10 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const Rest = require("../model/Resmodel");
-// const loginUserCtrl= require('../controller/userCtrl')
+const {createUser, loginResCtrl}= require('../controller/ResCtrl')
 
 const router = express.Router();
 router.post("/register", createUser);
-router.post("/login", loginUserCtrl);
-router.post("/logout", logoutUserCtrl);
+router.post("/login", loginResCtrl);
 
 module.exports = router;
