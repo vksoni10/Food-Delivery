766b469527040ac0279583971616759f981ee23d
garg10aditya <117253638+garg10aditya@users.noreply.github.com>
2024-07-11 11:21:36 +0530

    huhaa

--- backend/routes/restRoute.js
+++ backend/routes/restRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { createUser, loginResCtrl } = require("../controller/ResCtrl");
+// const { createUser, loginResCtrl } = require("../controller/ResCtrl");
 const Rest = require("../model/Resmodel");
 const {
   createUser,
