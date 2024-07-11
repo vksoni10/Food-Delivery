6f94d95a5934df0ccf03549ff12fd9eb8a1801fe
vksoni10 <vanshksoni@gmail.com>
2024-07-11 15:41:36 +0530

    menu

--- frontend/src/components/Restaurant/Menu.js
+++ frontend/src/components/Restaurant/Menu.js
@@ -60,58 +60,61 @@ export default function Menu() {
         </table>
       </div>
     </>
-// src/pages/RegisterRestaurant.js
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+    // src/pages/RegisterRestaurant.js
+    // import React, { useState } from 'react';
+    //   import axios from 'axios';
+    //   import { useNavigate } from 'react-router-dom';
 
-const Menu = () => {
- 
-  const [menu, setMenu] = useState([{ dishName: '', price: '', dishImage: '' }]);
+    //   const Menu = () => {
 
-  const navigate = useNavigate();
+    //     const [menu, setMenu] = useState([{ dishName: '', price: '', dishImage: '' }]);
 
-  const handleMenuChange = (index, e) => {
-    const newMenu = [...menu];
-    newMenu[index][e.target.name] = e.target.value;
-    setMenu(newMenu);
-  };
+    //     const navigate = useNavigate();
 
-  const addMenuItem = () => {
-    setMenu([...menu, { dishName: '', price: '', dishImage: '' }]);
-  };
+    //     const handleMenuChange = (index, e) => {
+    //       const newMenu = [...menu];
+    //       newMenu[index][e.target.name] = e.target.value;
+    //       setMenu(newMenu);
+    //     };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios.post('http://localhost:3001/Restaurant/menu', { 
-      menu
-    })
-    .then((result) => {
-      console.log(result);
-      navigate('/Restaurant/login');
-    })
-    .catch((err) => {
-      if (err.response && err.response.status === 400) {
-        alert(err.response.data.message);
-      } else {
-        console.error(err);
-      }
-    });
-  };
+    //     const addMenuItem = () => {
+    //       setMenu([...menu, { dishName: '', price: '', dishImage: '' }]);
+    //     };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      
-      {menu.map((item, index) => (
-        <div key={index}>
-          <input type="text" name="dishName" value={item.dishName} onChange={(e) => handleMenuChange(index, e)} placeholder="Dish Name" required />
-          <input type="number" name="price" value={item.price} onChange={(e) => handleMenuChange(index, e)} placeholder="Price" required />
-          <input type="text" name="dishImage" value={item.dishImage} onChange={(e) => handleMenuChange(index, e)} placeholder="Dish Image URL" required />
-        </div>
-      ))}
-      <button type="button" onClick={addMenuItem}>Add Menu Item</button>
+    //     const handleSubmit = (e) => {
+    //       e.preventDefault();
+    //       axios.post('http://localhost:3001/Restaurant/menu', {
+    //         menu
+    //       })
+    //         .then((result) => {
+    //           console.log(result);
+    //           navigate('/Restaurant/login');
+    //         })
+    //         .catch((err) => {
+    //           if (err.response && err.response.status === 400) {
+    //             alert(err.response.data.message);
+    //           } else {
+    //             console.error(err);
+    //           }
+    //         });
+    //     };
 
-      <button type="submit">Register Restaurant</button>
-    </form>
+    //     return (
+    //       <form onSubmit={handleSubmit}>
+
+    //         {menu.map((item, index) => (
+    //           <div key={index}>
+    //             <input type="text" name="dishName" value={item.dishName} onChange={(e) => handleMenuChange(index, e)} placeholder="Dish Name" required />
+    //             <input type="number" name="price" value={item.price} onChange={(e) => handleMenuChange(index, e)} placeholder="Price" required />
+    //             <input type="text" name="dishImage" value={item.dishImage} onChange={(e) => handleMenuChange(index, e)} placeholder="Dish Image URL" required />
+    //           </div>
+    //         ))}
+    //         <button type="button" onClick={addMenuItem}>Add Menu Item</button>
+
+    //         <button type="submit">Register Restaurant</button>
+    //       </form>
+    //     );
+    //   }
+    // }
   );
 }
