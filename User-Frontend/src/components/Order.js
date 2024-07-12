d06db7178863f1f4d7cb5eb61d6fdafee8f8a1dd
sparshj123 <f20220360@pilani.bits-pilani.ac.in>
2024-07-12 15:19:42 +0530

    sparsh

--- User-Frontend/src/components/Order.js
+++ User-Frontend/src/components/Order.js
@@ -6,12 +6,12 @@ const Order = ({ restaurant }) => {
   const [cart, setCart] = useState([]);
   const [itemInCart, setItemInCart] = useState({});
 
-  useEffect(() => {
-    // Fetch cart items from the backend on component mount
-    axios.get('http://localhost:3001/cart')
-      .then((response) => setCart(response.data))
-      .catch((error) => console.error('Error fetching cart:', error));
-  }, []);
+  // useEffect(() => {
+  //   // Fetch cart items from the backend on component mount
+  //   axios.get('http://localhost:3001/cart/getCart')
+  //     .then((response) => setCart(response.data))
+  //     .catch((error) => console.error('Error fetching cart:', error));
+  // }, []);
 
   const handleAddToCart = async (item) => {
     try {
