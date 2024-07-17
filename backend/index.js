2a8622aca0058921b1340e470b40baa3d00c5a4d
garg10aditya <117253638+garg10aditya@users.noreply.github.com>
2024-07-17 11:31:02 +0530

    countdown final pushes

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true, // Important
   })
