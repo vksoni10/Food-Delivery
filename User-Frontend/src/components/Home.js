1f5fbdab0436d031e1724c6e2684d84b9ee8f888
garg10aditya <117253638+garg10aditya@users.noreply.github.com>
2024-07-17 11:20:04 +0530

    final commit

--- User-Frontend/src/components/Home.js
+++ User-Frontend/src/components/Home.js
@@ -23,11 +23,9 @@ export default function Home() {
       <div className="content">
         <h1>POTATO</h1>
         <h4>Discover the best food and drinks in your area</h4>
-        
+                    <h4>For the foodies!</h4>        
                     <NavLink className="button style" to='/auth/restaurants'>Order Now</NavLink>
-
-         
-        
+                    
       </div>
     </div>
     </>
