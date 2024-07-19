d4da0c8d7febdf61c162355b72ad203a1507d30a
hiteshyadav886 <hiteshyadav886@gmail.com>
2024-07-19 11:41:22 +0530

    a

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const {getUserProfile} = require('../controller/UserAdmin')
 const {getRestProfile, deleteRestProfile} = require('../controller/RestAdmin')
-const AdminControl = require("../controller/AdminControl");
+const {getAllUserOrders} = require('../controller/AdminOrderController');
+// const AdminControl = require("../controller/AdminControl");
 
 const authMiddleware = require('../middleware/AdminMiddleware');
 
@@ -10,8 +11,9 @@ const router = express.Router();
 router.get('/users', getUserProfile);
 router.get('/restaurantslist', getRestProfile);
 router.delete('/deleteRestaurant/:id', deleteRestProfile);
-router.post("/signup", AdminControl.register);
-router.post("/login", AdminControl.login);
+// router.post("/signup", AdminControl.register);
+// router.post("/login", AdminControl.login);
+router.get('/all', getAllUserOrders);
 router.get("/dashboard", authMiddleware, (req, res) => {
   res.send("Welcome to the dashboard");   
   });
